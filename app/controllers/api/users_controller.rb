b9f52bd55f2cfa939206f7757a940e1e47a8e453
mikeevans85 <baudecordier@gmail.com>
2018-10-24 18:34:59 -0500

    Added user_id column to contacts database and added sessions controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::UsersController < ApplicationController
   def create
-    @user = User.new(
+    user = User.new(
       name: params["name"],
       email: params["email"],
       password: params["password"],
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,8 @@ class Contact < ApplicationRecord
   validates :name, :email, presence: true
   validates :email, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "only allows valid emails" }
+
+  belongs_to :user
   
   def friendly_updated_at
     updated_at.strftime("%D, %I:%M")
